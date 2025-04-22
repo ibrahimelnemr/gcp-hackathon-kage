5f1080fb61612274ca2e6e338bb0f0e80373a86b
El Nemr, Ibrahim <70034333+ibrahimelnemr@users.noreply.github.com>
2025-04-22 11:52:02 +0200

    ai chat endpoint working

--- backend_django/api/views/ai.py
+++ backend_django/api/views/ai.py
@@ -20,15 +20,43 @@ client = genai.Client(
 
 model = GenerativeModel("gemini-pro")
 
+# @api_view(['GET'])
+# def ai_chat(request):
+#     try:
+#         user_input = "Hello"
+#         if not user_input:
+#             return JsonResponse({"error": "Missing 'message' in request body."}, status=400)
+
+#         response = model.generate_content(user_input)
+#         return JsonResponse({"response": response.text})
+
+#     except Exception as e:
+#         return JsonResponse({"error": str(e)}, status=500)
+    
+
 @api_view(['GET'])
 def ai_chat(request):
     try:
-        user_input = "Hello"
-        if not user_input:
-            return JsonResponse({"error": "Missing 'message' in request body."}, status=400)
+        model = "gemini-1.5-pro-002"
+        response = client.models.generate_content(
+        model=model,
+        contents=[
+            "Hello"
+        ],
+        )
+        print(response.text, end="")
 
-        response = model.generate_content(user_input)
-        return JsonResponse({"response": response.text})
+        # Initialize the Vertex AI client with your project and location
+        aiplatform.init(project='centered-accord-442214-b9', location='us-central1')
+
+        models = aiplatform.Model.list()  
+
+        model_names = [model.display_name for model in models]
+
+        # Return model names or other relevant info
+        return Response(response.text, status=200)
 
     except Exception as e:
+        # return {"error": str(e)}
+        # return Response({"error": str(e)}, status=500)
         return JsonResponse({"error": str(e)}, status=500)

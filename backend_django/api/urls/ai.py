03047629890c66373724e8c0b153fe4b7eca9a4e
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-04-23 18:11:21 +0200

    Update ai.py

--- backend_django/api/urls/ai.py
+++ backend_django/api/urls/ai.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from ..views.ai import ai_chat
+from ..views.ai import *
 
 app_name = 'ai'
 
 urlpatterns = [
     path('ai/check', ai_chat, name='ai_chat'),
+    path('ai/generate', generate_project_plan, name='ai_generate')
 ]

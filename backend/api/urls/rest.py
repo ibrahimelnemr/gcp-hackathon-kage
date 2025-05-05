22512e82851d04bb1a64fa0082ec20f44fbadcdc
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-05-05 10:25:34 +0300

    add employee rest endpoints

--- backend/api/urls/rest.py
+++ backend/api/urls/rest.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ..views.project import ProjectViewSet
 from ..views.task import TaskViewSet
+from ..views.employee import EmployeeViewSet 
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'employees', EmployeeViewSet, basename='employee') 
 
 urlpatterns = [
     path('rest/', include(router.urls)),
